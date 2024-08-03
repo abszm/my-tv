bd8b8865d560bb9d0836d0fe1375f4bca93566d1
abszm <ytb0325@outlook.com>
2024-08-03 15:49:35 +0800

    test port

--- app/src/main/java/com/lizongying/mytv0/PortUtil.kt
+++ app/src/main/java/com/lizongying/mytv0/PortUtil.kt
@@ -1,39 +1,43 @@
-package com.lizongying.mytv0;
+package com.lizongying.mytv0
 
-import java.io.IOException;
-import java.net.Inet4Address;
-import java.net.NetworkInterface;
-import java.net.ServerSocket;
+import java.io.IOException
+import java.net.Inet4Address
+import java.net.NetworkInterface
+import java.net.ServerSocket
 
 object PortUtil {
 
-    public int findFreePort() {
-        int port = 36185;
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(port);
-            return port;
-        } catch (IOException e) {
-            System.out.println("端口[" + port + "]被占用，程序无法正常启动！");
-            return -1;
-        } finally {
-            if (serverSocket!= null) {
-                try {
-                    serverSocket.close();
-                } catch (IOException e) {
-                    System.out.println("关闭端口[" + port + "]失败！");
+    fun findFreePort(): Int {
+        return try {
+            ServerSocket(36185).use { socket ->
+                socket.localPort
+            }
+        } catch (e: IOException) {
+            try {
+                ServerSocket(0).use { socket ->
+                    socket.localPort
                 }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                -1 // Return -1 to indicate an error
             }
         }
     }
 
-    public String lan() {
-        try {
-            InetAddress address = InetAddress.getLocalHost();
-            return address.getHostAddress();
-        } catch (Exception e) {
-            System.out.println("获取IPv4地址失败！");
-            return null;
+    fun lan(): String? {
+        val networkInterfaces = NetworkInterface.getNetworkInterfaces()
+        while (networkInterfaces.hasMoreElements()) {
+            val inetAddresses = networkInterfaces.nextElement().inetAddresses
+            while (inetAddresses.hasMoreElements()) {
+                val inetAddress = inetAddresses.nextElement()
+                if (inetAddress is Inet4Address) {
+                    if (inetAddress.hostAddress == "127.0.0.1") {
+                        continue
+                    }
+                    return inetAddress.hostAddress
+                }
+            }
         }
+        return null
     }
 }
